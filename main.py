9ec9010215cb648c230d34880e6bd8f3106eb321
Ken Valverde <verify>
2020-08-01 18:45:36 -0500

    reversed word variable and checked if equal to original

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ time.sleep(1)
 
 # Ask user to input word
 word = input('Enter a word: ').casefold()  # casefold() ignores casing
+print("...")
+time.sleep(1)
 
 # reverse the string
+reverse_word = reversed(word)
 
+# check if the string is equal to its reverse
+if list(word) == list(reverse_word):
+    print("This is a Palindrome!")
+else:
+    print("This is not a Palindrome!")
