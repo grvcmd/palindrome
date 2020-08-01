37eb0973eb0d1ee4b0e4535a8291304a9753e348
Ken Valverde <verify>
2020-08-01 18:39:03 -0500

    created word variable for user input

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Palindrome
+# Program to check if a string is palindrome or not
+import time
+
+print("Let's see if your word is a palindrome!")
+time.sleep(1)
+
+# Ask user to input word
+word = input('Enter a word: ').casefold()  # casefold() ignores casing
+
+# reverse the string
 
